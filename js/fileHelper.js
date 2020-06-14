6cbaf28373ce501a323a317babf952a6d59a3b31
maxfer1221 <63942620+maxfer1221@users.noreply.github.com>
2020-06-14 03:20:48 -0400

    Update fileHelper.js

--- js/fileHelper.js
+++ js/fileHelper.js
@@ -1,7 +1,8 @@
-getData();
+var global_article_list = getData()
+
+console.log(global_article_list)
 
 async function getData(){
     const response = await fetch('csv/blacklivesmatter_05-25_06-12_topTweets_urlSummary_test_file.csv');
-    const data = await response.json();
-    console.log(data);
+    return await response.text();
 }
