312a7dc5420e4bfa5b14db7a425e7d5acb3aeef7
maxfer1221 <63942620+maxfer1221@users.noreply.github.com>
2020-06-14 04:26:07 -0400

    Update fileHelper.js

--- js/fileHelper.js
+++ js/fileHelper.js
@@ -1,22 +1,21 @@
 var global_article_list = getData('csv/blacklivesmatter_05-25_06-12_topTweets_urlSummary_test_file.csv')
 
-console.log(global_article_list)
-console.log(global_article_list)
-
-console.log(global_article_list)
-
-console.log(global_article_list)
-console.log(global_article_list)
-console.log(global_article_list)
-console.log(global_article_list)
-console.log(global_article_list)
-console.log(global_article_list)
-console.log(global_article_list)
-
-
 async function getData(url){
     const response = await fetch(url);
     const data = await response.text();
     console.log(data);
     return data;
 }
+
+console.log(global_article_list)
+console.log(global_article_list)
+
+console.log(global_article_list)
+
+console.log(global_article_list)
+console.log(global_article_list)
+console.log(global_article_list)
+console.log(global_article_list)
+console.log(global_article_list)
+console.log(global_article_list)
+console.log(global_article_list)
